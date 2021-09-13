5f01a4025dfc475b87fa6ef82390345a04af6bc0
VittoriaES <vittoes9@gmail.com>
2021-09-13 16:42:52 +0200

    NewPlatforms

--- Assets/#Projects/Scripts/KeyBehaviour.cs
+++ Assets/#Projects/Scripts/KeyBehaviour.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class KeyBehaviour : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other) {
-        Destroy(this.gameObject);
+    public UnityEvent whenPickUp;
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")){
+            whenPickUp?.Invoke();
+            Destroy(gameObject);
+        }
     }
 }
